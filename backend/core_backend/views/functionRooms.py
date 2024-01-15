dec97ec0e7331a1abd8852d64ff3e757f2716c74
Diego RM <diegoromomuxoz@gmail.com>
2024-01-15 13:25:51 -0600

    Feat::sparkles: Create the api for users and rooms

---
+++ backend/core_backend/views/functionRooms.py
@@ -0,0 +1,79 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from core_backend.models import Sala
+from voluptuous.schema_builder import Required
+from backend.utils import validate_data
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_room (request):
+    rooms = Sala.objects.all()
+    response = []
+    for room in rooms:
+        response.append({
+            'id':room.id,
+            'nombre':room.nombre,
+            'tamano':room.tamano,
+            'ubicacion':room.ubicacion,
+            'aforo':room.aforo
+        })
+    return Response(response)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_room (request):
+    data = validate_data({
+        Required('nombre'):str,
+        ('tamano'):int,
+        ('ubicacion'):str,
+        ('aforo'):int,
+    },request.data)
+    if 'tamano' not in data:
+        data['tamano'] = None 
+    if 'ubicacion' not in data:
+        data['ubicacion'] = None
+    if 'aforo' not in data:
+        data['aforo']  = None
+    room = Sala.objects.create(nombre=data['nombre'],tamano=data['tamano'],ubicacion=data['ubicacion'],aforo=data['aforo'])
+    room.save()
+    return Response('Sala creada con exito')
+
+@api_view(['PUT'])
+@permission_classes([AllowAny])
+def update_room (request):
+    data = validate_data({
+        Required('id'):int,
+        ('nombre'):str,
+        ('tamano'):int,
+        ('ubicacion'):str,
+        ('aforo'):int
+    },request.data)
+    try:
+        room=Sala.objects.get(id=data['id'])
+        if 'nombre' in data:
+            room.nombre=data['nombre']
+        if 'tamano' in data:
+            room.tamano=data['tamano']
+        if 'ubicacion' in data:
+            room.ubicacion=data['ubicacion']
+        if 'aforo' in data:
+            room.aforo=data['aforo']
+        room.save()
+        return Response('Sala actualizada con exito')
+    except: 
+        return Response('La sala que deseas modificar no existe')
+    
+@api_view(['DELETE'])
+@permission_classes([AllowAny])
+def delete_room(request):
+    data = validate_data({
+        Required('id'):int,
+    }, request.data)
+    try:
+        room = Sala.objects.get(id=data['id'])
+        room.delete()
+        return Response('Sala eliminada exitosamente')
+    except:
+        return Response('La sala que deseas eliminar no existe')
---
+++ backend/core_backend/views/functionUsers.py
@@ -0,0 +1,94 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from core_backend.models import Usuario
+from voluptuous.schema_builder import Required
+from backend.utils import validate_data 
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_user (request):
+    users = Usuario.objects.all()
+    response = []
+    for user in users:
+        response.append({
+            'id':user.id,
+            'nombre':user.nombre,
+            'apellido':user.apellido,
+            'identificador':user.identificador,
+        })
+    return Response(response)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_user_id (request):
+    response = []
+    data = validate_data({
+        Required('identificador'):int,
+    },request.data)
+    try:
+        user=Usuario.objects.get(identificador=data['identificador'])
+        response.append({
+            'id':user.id,
+            'nombre':user.nombre,
+            'apellido':user.apellido,
+            'identificador':user.identificador,
+        })
+        return Response(response)
+    except:
+        return Response('Usuario no encontrado')
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_user (request):
+    data = validate_data({
+        Required('nombre'):str,
+        ('apellido'):str,
+        #('identificador'):int,
+    },request.data)
+    if 'apellido' not in data:
+        data['apellido'] = None 
+
+    # Obtener el último id de Usuario
+    last_user_id = Usuario.objects.latest('id').id if Usuario.objects.exists() else 0
+    # Calcular el nuevo identificador sumándole 1000
+    new_identifier = last_user_id + 1000
+    # Crear la instancia de Usuario con el nuevo identificador
+    user = Usuario.objects.create(nombre=data['nombre'], apellido=data['apellido'], identificador=new_identifier)
+
+    #user = Usuario.objects.create(nombre=data['nombre'],apellido=data['apellido'],identificador=id+1000)
+    user.save()
+    return Response('Usuario creado con exito')
+
+@api_view(['PUT'])
+@permission_classes([AllowAny])
+def update_user (request):
+    data = validate_data({
+        Required('identificador'):int,
+        ('nombre'):str,
+        ('apellido'):int,
+    },request.data)
+    try:
+        user=Usuario.objects.get(identificador=data['identificador'])
+        if 'nombre' in data:
+            user.nombre=data['nombre']
+        if 'apellido' in data:
+            user.apellido=data['apellido']
+        user.save()
+        return Response('Usuario actualizado con exito')
+    except: 
+        return Response('El usuario que deseas modificar no existe')
+
+@api_view(['DELETE'])
+@permission_classes([AllowAny])
+def delete_user(request):
+    data = validate_data({
+        Required('identificador'):int,
+    }, request.data)
+    try:
+        user = Usuario.objects.get(identificador=data['identificador'])
+        user.delete()
+        return Response('Usuario eliminado exitosamente')
+    except:
+        return Response('El usuario que deseas eliminar no existe')
